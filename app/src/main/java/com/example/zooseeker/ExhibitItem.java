36886676a27024fda59851d37578bc0526b27b0c
Ziv Fuchs <ziv.fuchs@gmail.com>
2022-04-29 16:46:28 -0700

    Exhibit List Recycler View

--- app/src/main/java/com/example/zooseeker/ExhibitItem.java
+++ app/src/main/java/com/example/zooseeker/ExhibitItem.java
@@ -1,5 +1,15 @@
 package com.example.zooseeker;
 
-public class ExhibitItem {
+import java.util.List;
 
+public class ExhibitItem {
+    public long id;
+    public String name;
+    public List<String> tags;
+
+    public ExhibitItem(long id, String name, List<String> tags) {
+        this.id = id;
+        this.name = name;
+        this.tags = tags;
+    }
 }
--- app/src/main/java/com/example/zooseeker/ExhibitListAdapter.java
+++ app/src/main/java/com/example/zooseeker/ExhibitListAdapter.java
@@ -1,37 +1,64 @@
 package com.example.zooseeker;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ExhibitListAdapter extends RecyclerView.Adapter<ExhibitListAdapter.ViewHolder> {
+import java.util.Collections;
+import java.util.List;
 
+public class ExhibitListAdapter extends RecyclerView.Adapter<ExhibitListAdapter.ViewHolder> {
+    private List<ExhibitItem> exhibitItems = Collections.emptyList();
+
+    public void setExhibitListItems(List<ExhibitItem> newExhibitItems) {
+        this.exhibitItems.clear();
+        this.exhibitItems = newExhibitItems;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.exhibit_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.setExhibitItem(exhibitItems.get(position));
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public int getItemCount() { return exhibitItems.size(); }
+
+    @Override
+    public long getItemId(int position) { return exhibitItems.get(position).id; }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
+        private final CheckBox checkBox;
         private ExhibitItem exhibitItem;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+             this.textView = itemView.findViewById(R.id.exhibit_item_text);
+             this.checkBox = itemView.findViewById(R.id.exhibit_item_checkBox);
+
+        }
+
+        public ExhibitItem getExhibitItem() {return exhibitItem;}
+
+        public void setExhibitItem(ExhibitItem exhibitItem) {
+            this.exhibitItem = exhibitItem;
+            this.textView.setText(exhibitItem.name);
         }
     }
 }
