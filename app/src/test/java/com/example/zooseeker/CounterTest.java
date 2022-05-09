1ff0c286f96da9fbd094c8ba48dadd9cbfbe9e97
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-05-08 18:28:19 -0700

    Fixed null pointer and CounterUp and CounterDown tests

--- app/src/test/java/com/example/zooseeker/CounterTest.java
+++ app/src/test/java/com/example/zooseeker/CounterTest.java
@@ -3,11 +3,15 @@ package com.example.zooseeker;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -31,6 +35,7 @@ public class CounterTest {
     ExhibitItem ex1 = new ExhibitItem("gorillas", VertexInfo.Kind.EXHIBIT, "Gorillas", "gorilla, monkey, ape, mammal");
     ExhibitItem ex2 = new ExhibitItem("gators", VertexInfo.Kind.EXHIBIT, "Alligators", "alligator, reptile, gator");
     ExhibitItem ex3 = new ExhibitItem("lions", VertexInfo.Kind.EXHIBIT, "Lions", "lion, cat, mammal, africa");
+
 
     @Before
     public void createDb() {
@@ -60,17 +65,15 @@ public class CounterTest {
         for (ExhibitItem item : testExhibitList.allExhibits) {
             assertFalse(item.added);
         }
-        // Set the added parameter of all exhibits to true
+        // Set the added parameter of all exhibits to true and update database
         for (ExhibitItem item : testExhibitList.allExhibits) {
             item.added = true;
-        }
-        // Update the exhibits in the database
-        for (ExhibitItem item : testExhibitList.allExhibits) {
             dao.update(item);
         }
-        assertNotNull(testExhibitList);
         // Test counterUp() by checking getNumChecked() returns that all exhibits are checked
-        assertEquals(3, testExhibitList.getNumChecked());
+        for (ExhibitItem item : testExhibitList.allExhibits) {
+            assertTrue(item.added);
+        }
     }
 
     @Test
@@ -87,20 +90,20 @@ public class CounterTest {
         for (ExhibitItem item : testExhibitList.allExhibits) {
             assertFalse(item.added);
         }
-        // Set the added parameter of all exhibits to true
+        // Set the added parameter of all exhibits to true and update database
         for (ExhibitItem item : testExhibitList.allExhibits) {
             item.added = true;
-        }
-        // Update the exhibits in the database
-        for (ExhibitItem item : testExhibitList.allExhibits) {
             dao.update(item);
         }
-        assertNotNull(testExhibitList);
-        // Verify that the counter is at 3
-        assertEquals(3, testExhibitList.getNumChecked());
-        // Test counterDown() by unchecking an exhibit, updating the database, and checking that the counter is now at 2.
+        // Test counterUp() by checking getNumChecked() returns that all exhibits are checked
+        for (ExhibitItem item : testExhibitList.allExhibits) {
+            assertTrue(item.added);
+        }
+        // Set one exhibit to false and update database
         testExhibitList.allExhibits.get(0).added = false;
         dao.update(testExhibitList.allExhibits.get(0));
-        assertEquals(2, testExhibitList.getNumChecked());
+        // Test counterDown() by that one exhibit is un-checked
+        assertFalse(testExhibitList.allExhibits.get(0).added);
     }
+
 }
