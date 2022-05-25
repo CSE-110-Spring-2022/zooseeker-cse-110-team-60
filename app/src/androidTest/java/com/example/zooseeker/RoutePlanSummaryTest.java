816b88e57415add1756f2a948348eca70960099e
alexbastuba <abastuba@ucsd.edu>
2022-05-24 18:23:47 -0700

    RoutePlanSummaryTest.java refactored.

--- app/src/androidTest/java/com/example/zooseeker/RoutePlanSummaryTest.java
+++ app/src/androidTest/java/com/example/zooseeker/RoutePlanSummaryTest.java
@@ -5,7 +5,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -13,6 +12,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +26,6 @@ import androidx.test.filters.LargeTest;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,7 +41,7 @@ public class RoutePlanSummaryTest {
     @Test
     public void routePlanSummaryTest() {
         // Click "Clear" button to reset selected exhibits
-        onView(allOf(withId(R.id.clearExhibitsBtn), withText("Clear"),
+        onView(allOf(withId(R.id.main_clear_button), withText("Clear"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
@@ -52,7 +51,7 @@ public class RoutePlanSummaryTest {
 
 
         // Click directions button without exhibits selected
-        onView(allOf(withId(R.id.getDirectionsButton), withText("Get Directions"),
+        onView(allOf(withId(R.id.main_getDirections_button), withText("Get Directions"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
@@ -75,27 +74,27 @@ public class RoutePlanSummaryTest {
 
         // Select Gorillas and Alligators, Click "Get Directions" button
         ViewInteraction materialCheckBox = onView(
-                allOf(withId(R.id.added),
+                allOf(withId(R.id.exhibit_item_checkBox),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.rvExhibits),
+                                        withId(R.id.main_exhibits_recyclerView),
                                         0),
                                 0),
                         isDisplayed()));
         materialCheckBox.perform(click());
 
         ViewInteraction materialCheckBox2 = onView(
-                allOf(withId(R.id.added),
+                allOf(withId(R.id.exhibit_item_checkBox),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.rvExhibits),
+                                        withId(R.id.main_exhibits_recyclerView),
                                         3),
                                 0),
                         isDisplayed()));
         materialCheckBox2.perform(click());
 
         ViewInteraction materialButton3 = onView(
-                allOf(withId(R.id.getDirectionsButton), withText("Get Directions"),
+                allOf(withId(R.id.main_getDirections_button), withText("Get Directions"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
@@ -105,19 +104,19 @@ public class RoutePlanSummaryTest {
         materialButton3.perform(click());
 
         // Assert "Gate to Alligators" on Summary RecyclerView
-        onView(allOf(withId(R.id.summary_item_text), withText("Gate to Alligators (110 meters)"),
+        onView(allOf(withId(R.id.summary_item_text), withText("Gate to Alligators (110.0 meters)"),
                         withParent(withParent(withId(R.id.direction_items))),
-                        isDisplayed())).check(matches(withText("Gate to Alligators (110 meters)")));
+                        isDisplayed())).check(matches(withText("Gate to Alligators (110.0 meters)")));
 
         //Assert "Alligators to Gorillas" on Summary RecyclerView
-        onView(allOf(withId(R.id.summary_item_text), withText("Alligators to Gorillas (300 meters)"),
+        onView(allOf(withId(R.id.summary_item_text), withText("Alligators to Gorillas (300.0 meters)"),
                         withParent(withParent(withId(R.id.direction_items))),
-                        isDisplayed())).check(matches(withText("Alligators to Gorillas (300 meters)")));
+                        isDisplayed())).check(matches(withText("Alligators to Gorillas (300.0 meters)")));
 
         //Assert "Gorillas to Gate" on Summary RecyclerView
-        onView(allOf(withId(R.id.summary_item_text), withText("Gorillas to Gate (210 meters)"),
+        onView(allOf(withId(R.id.summary_item_text), withText("Gorillas to Gate (210.0 meters)"),
                         withParent(withParent(withId(R.id.direction_items))),
-                        isDisplayed())).check(matches(withText("Gorillas to Gate (210 meters)")));
+                        isDisplayed())).check(matches(withText("Gorillas to Gate (210.0 meters)")));
 
         // Assert that "Back" button is on summary screen
         onView(allOf(withId(R.id.back_btn), withText("BACK"),
@@ -138,63 +137,43 @@ public class RoutePlanSummaryTest {
                                 1),
                         isDisplayed())).perform(click());
 
-        // TO DO: Check checkboxes checkled, use the checkbox test on main to see how to find the ViewInteraction
-        //Assert "Alligators to Gorillas" on Summary RecyclerView
-        onView(allOf(withId(R.id.added), withText("Alligators"),
-                withParent(withParent(withId(R.id.rvExhibits))),
-                isDisplayed())).check(matches(withText("Alligators to Gorillas (300 meters)")));
-        ViewInteraction checkBox = onView(
-                allOf(withId(R.id.added),
-                        withParent(withParent(withId(R.id.rvExhibits))),
-                        isDisplayed()));
-        checkBox.check(matches(isDisplayed()));
-
-        ViewInteraction checkBox2 = onView(
-                allOf(withId(R.id.added),
-                        withParent(withParent(withId(R.id.rvExhibits))),
-                        isDisplayed()));
-        checkBox2.check(matches(isDisplayed()));
-
-        ViewInteraction materialButton5 = onView(
-                allOf(withId(R.id.getDirectionsButton), withText("Get Directions"),
+        // Click "Get Directions" from search screen to return to summary.
+        onView(allOf(withId(R.id.main_getDirections_button), withText("Get Directions"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
                                 8),
-                        isDisplayed()));
-        materialButton5.perform(click());
+                        isDisplayed())).perform(click());
 
-        ViewInteraction materialButton6 = onView(
-                allOf(withId(R.id.go_btn), withText("GO"),
+        // From summary screen, click "Go"
+        onView(allOf(withId(R.id.go_btn), withText("GO"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
                                 2),
-                        isDisplayed()));
-        materialButton6.perform(click());
+                        isDisplayed())).perform(click());
 
-        ViewInteraction textView5 = onView(
-                allOf(withId(R.id.header_txt), withText("Gate to Alligators"),
+        onView(
+                allOf(withId(R.id.direction_header_textView), withText("Entrance and Exit Gate to Alligators (110.0m)"),
                         withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView5.check(matches(withText("Gate to Alligators")));
+                        isDisplayed())).check(matches(withText("Entrance and Exit Gate to Alligators (110.0m)")));
 
         ViewInteraction button3 = onView(
-                allOf(withId(R.id.previous_btn), withText("PREVIOUS"),
+                allOf(withId(R.id.direction_previous_button), withText("PREVIOUS"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         button3.check(matches(isDisplayed()));
 
         ViewInteraction button4 = onView(
-                allOf(withId(R.id.exit_btn), withText("EXIT"),
+                allOf(withId(R.id.direction_exit_button), withText("EXIT"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         button4.check(matches(isDisplayed()));
 
         ViewInteraction button5 = onView(
-                allOf(withId(R.id.next_btn), withText("NEXT"),
+                allOf(withId(R.id.direction_next_button), withText("NEXT"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         button5.check(matches(isDisplayed()));
