c443e6de29868347133a2589ce6715f603effffc
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-05-30 19:13:22 -0700

    Added comments #5

--- app/src/test/java/com/example/zooseeker/CheckBoxTest.java
+++ app/src/test/java/com/example/zooseeker/CheckBoxTest.java
@@ -16,24 +16,32 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class CheckBoxTest {
+    /**
+     * Name:     testCheckBox
+     * Behavior: Verify that the checkBox returns True after the first checkbox is set to true.
+     *           Then, set the checkBox to False and verify that the first checkbox is set to false.
+     *
+     */
     @Test
     public void testCheckBox() {
-        Boolean completed = true;
+        // Start an instance of main activity for testing.
         ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
         scenario.moveToState(Lifecycle.State.RESUMED);
 
         scenario.onActivity(activity -> {
+            // Create a recyclerView and set it to the first checkbox on the screen.
             RecyclerView recyclerView = activity.recyclerView;
             RecyclerView.ViewHolder firstVH = recyclerView.findViewHolderForAdapterPosition(0);
             assertNotNull(firstVH);
-
             CheckBox checkBox = firstVH.itemView.findViewById(R.id.exhibit_item_checkBox);
-            checkBox.setChecked(true);
 
+            // Set the first checkbox to True and verify that it is true.
+            checkBox.setChecked(true);
             assertTrue(checkBox.isChecked());
 
+            // Set the second checkbox to False and verify that it is false.
             checkBox.setChecked(false);
             assertFalse(checkBox.isChecked());
         });
